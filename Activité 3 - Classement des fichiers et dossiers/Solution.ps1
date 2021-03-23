ccef9daba44d0a05b7c42138ec133fca9358960e
Chacky1 <th.alban@outlook.fr>
2021-03-23 18:39:29 +0100

    Ajout de l'activité 3 avec le dossier de tests

---
+++ Activité 3 - Classement des fichiers et dossiers/Solution.ps1
@@ -0,0 +1,10 @@
+﻿<#
+    .Description
+    Script qui affiche les fichiers et dossiers par date de modification descendante
+#>
+
+Param (
+    [String]$Path
+)
+
+Get-ChildItem -Path $Path | Sort-Object LastWriteTime -Descending | Select-Object Fullname,LastWriteTime | Write-Output
