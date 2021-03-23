4b55ebca7f0a0f6c9ed5427e495c6070333b96a2
Chacky1 <th.alban@outlook.fr>
2021-03-23 18:08:56 +0100

    Ajout de l'activité 4

---
+++ Activité 4 - Boucle for à conditions multiples/solution.ps1
@@ -0,0 +1,11 @@
+﻿<#
+    .Description
+    Affiche le top 10 des langages stockés dans la variable langages ci-dessous
+#>
+
+$langages = @("PowerShell", "Bash", ".NET", "Python", "C")
+
+For(($i = 1), ($j = 0); $i -le 10 -and $j -lt $langages.Length; $i++, $j++)
+{
+    Write-Output "$i - $($langages[$j])"
+}
